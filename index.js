051ab2871a781045357a05a7e3ef5b824a7ba67c
Rimon Zaya <n01514083@humber.ca>
2024-03-21 16:48:50 -0400

    Description of changes3

--- index.js
+++ index.js
@@ -1,10 +1,21 @@
 const express = require('express');
 const mongoose = require('mongoose');
-
+const path = require('path'); 
 const app = express();
+const bodyParser = require('body-parser');
 const PORT = process.env.PORT || 3000;
 const usersRouter = require('./routes/clients');
 require('dotenv').config(); // Load environment variables from .env file
+
+
+
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+// Set Pug as the view engine
+app.set('view engine', 'pug');
+app.set('views', path.join(__dirname, 'views')); // Set the directory where your views will be stored
 
 
 app.use('/client', usersRouter);
--- routes/clients.js
+++ routes/clients.js
@@ -1,16 +1,65 @@
 // routes/users.js
 const express = require('express');
 const router = express.Router();
-const Client = require('../models/client');
-
+const Laser = require('../models/laser');
+const Client = require("../models/client")
 // Define routes
-router.get('/', async (req, res) => {
-  try {
-    const clients = await Client.find();
-    res.json(clients);
-  } catch (err) {
-    res.status(500).json({ message: err.message });
-  }
+router.get('/', async (req, res, next) => {
+    try {
+        const lasers = await Laser.find();
+        return res.render('homePage', { lasers }); // Pass laser data to the Pug template
+    } catch (error) {
+        console.error(error);
+        return res.render('error', { error });
+    }
+});
+
+
+router.get('/book/:id', async (req, res, next) => {
+    try {
+        const laser = await Laser.findById(req.params.id);
+        if (!laser) {
+            return res.status(404).send("Laser service not found");
+        }
+        // Render the booking page with the laser information
+        return res.render('bookingPage', { laser });
+    } catch (error) {
+        console.error(error);
+        return res.render('error', { error });
+    }
+});
+
+
+
+// POST route to process booking form data
+router.post('/book/:id', async (req, res, next) => {
+    try {
+        const laser = await Laser.findById(req.params.id);
+        if (!laser) {
+            return res.status(404).send("Laser service not found");
+        }
+
+        // Create a new instance of the Client model with form data
+        const newClient = new Client({
+            firstName: req.body.firstName,
+            lastName: req.body.lastName,
+            phoneNumber: req.body.phoneNumber,
+            email: req.body.email,
+            appointmentDate: req.body.appointmentDate,
+            laserName: laser.laserName,
+            laserPrice: laser.laserPrice,
+            laserTime: laser.laserTime
+        });
+
+        // Save the new client data to the database
+        await newClient.save();
+
+        // Redirect to a success page after booking
+        return res.redirect('/success');
+    } catch (error) {
+        console.error(error);
+        return res.render('error', { error });
+    }
 });
 
 module.exports = router;
